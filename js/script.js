06f321117815736e07a8de28c3d10d14d28c3188
ELSON-341 <elson.sn314@gmail.com>
2024-04-05 14:57:24 -0300

    js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict'
+// Seleção de Elementos
+const geneatePassworBtn = document.getElementById('generate-password')
+const geneatedPassword = document.getElementById('generated-password')
+// Funções
+const getLetterLowerCase = () => {
+    console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 97)); 
+}
+
+getLetterLowerCase()
+
+// Eventos
+geneatePassworBtn.addEventListener('click', (e) => {
+    console.log('test');
+})
