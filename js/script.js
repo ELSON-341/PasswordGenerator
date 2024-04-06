1178c1762c79f27c5e813203793209d22f521d43
ELSON-341 <elson.sn314@gmail.com>
2024-04-06 09:24:14 -0300

    generating rondom number

--- js/script.js
+++ js/script.js
@@ -4,10 +4,23 @@ const geneatePassworBtn = document.getElementById('generate-password')
 const geneatedPassword = document.getElementById('generated-password')
 // Funções
 const getLetterLowerCase = () => {
-    console.log(String.fromCharCode(Math.floor(Math.random() * 26) + 97)); 
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
 }
 
-getLetterLowerCase()
+const getUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65)
+}
+
+const getNumber = () => {
+    return String(Math.floor(Math.random() * 10)).toString()
+}
+
+const getSymbol = () =>{
+    const symbol = '(){}[];=,<>.!?+-:%$#@*&/'
+    return symbol[Math.floor(Math.random() * symbol.length)]
+}
+
+console.log(getSymbol());
 
 // Eventos
 geneatePassworBtn.addEventListener('click', (e) => {
