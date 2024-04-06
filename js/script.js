f084fe5b3aefee09d96140a79a4a00aff4987d82
ELSON-341 <elson.sn314@gmail.com>
2024-04-06 09:35:36 -0300

    js

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 // Seleção de Elementos
 const geneatePassworBtn = document.getElementById('generate-password')
 const geneatedPassword = document.getElementById('generated-password')
+
 // Funções
 const getLetterLowerCase = () => {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97)
@@ -19,8 +20,6 @@ const getSymbol = () =>{
     const symbol = '(){}[];=,<>.!?+-:%$#@*&/'
     return symbol[Math.floor(Math.random() * symbol.length)]
 }
-
-console.log(getSymbol());
 
 // Eventos
 geneatePassworBtn.addEventListener('click', (e) => {
